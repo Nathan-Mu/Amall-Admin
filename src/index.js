564d2c811d955b2a7ef8089d5f40759de60ad879
Nathan <zhaody.mu@gmail.com>
2021-03-22 00:23:45 +1100

    change to hash router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from 'Redux/store.js';
@@ -8,9 +8,9 @@ import store from 'Redux/store.js';
 
 ReactDOM.render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>,
 	document.getElementById('root')
 );
